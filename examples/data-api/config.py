0499023db64d057e12007b31663207498460f2c8
NgTGiang <92700573+NgTGiang@users.noreply.github.com>
2025-11-23 20:38:31 +0700

    Revert "Add get intradate data function"

--- examples/data-api/config.py
+++
@@ -1,6 +0,0 @@
-auth_type = 'Bearer'
-consumerID = '8e319f551ebe4e3eb576e17735e9bc8c'
-consumerSecret = '488d02ef3e4049529b8ccacffa244cf8'
-
-url = 'https://fc-data.ssi.com.vn/'
-stream_url = 'https://fc-datahub.ssi.com.vn/'
